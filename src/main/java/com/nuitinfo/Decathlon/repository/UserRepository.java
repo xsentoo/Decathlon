64da66342cba7345ed29d6ea8ebb108a1fc62e88
senthooran <123653047+xsentoo@users.noreply.github.com>
2025-12-04 22:03:54 +0100

    Structure edit2

--- src/main/java/com/nuitinfo/Decathlon/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nuitinfo.Decathlon.repository;
-
-import com.nuitinfo.Decathlon.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
